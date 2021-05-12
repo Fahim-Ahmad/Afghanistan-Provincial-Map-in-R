b81941f25dce95145005b973d5a59aa61b043f07
Fahim Ahmad <fyousufzai93@gmail.com>
2021-05-12 14:27:39 +0430

    Add files via upload

---
+++ script.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(sf)
+
+# unzip("input/afghan_province.zip", exdir = "input")
+map <- st_read("input/afghan_province.shp")
+map <- full_join(map, read.csv("input/map_labels.csv"), by = c("prov" = "province"))
+
+# simple map
+map %>% 
+  ggplot() +
+  geom_sf(fill = "skyblue", color = "gray50", show.legend = F, alpha = 0.5) +
+  geom_text(aes(position_x, position_y, label = prov), size = 3) +
+  theme_void() +
+  theme(plot.title = element_text(hjust = 0.5, size = 15)) +
+  labs(title = "Afghanistan Provincial Map", fill = NULL) +
+  NULL
+
+ggsave("output/map_1.png", width = 12, height = 8)
+
+map %>% 
+  ggplot() +
+  geom_sf(aes(fill = prov), color = "gray50", show.legend = F, alpha = 0.5) +
+  geom_text(aes(position_x, position_y, label = prov), size = 3) +
+  theme_void() +
+  theme(plot.title = element_text(hjust = 0.5, size = 15)) +
+  labs(title = "Afghanistan Provincial Map", fill = NULL) +
+  NULL
+
+ggsave("output/map2.png", width = 12, height = 8)
+
+
